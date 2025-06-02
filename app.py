1c93cc11eec9efc80d26edf015a74dc01d8d45ab
erkston <erkston@gmail.com>
2025-06-02 17:07:59 -0400

    Add full a2s info

--- app.py
+++ app.py
@@ -36,22 +36,7 @@ def query_server():
         ]
 
         result = {
-            "server_name": server_info.server_name,
-            "map_name": server_info.map_name,
-            "folder": server_info.folder,
-            "game": server_info.game,
-            "app_id": server_info.app_id,
-            "player_count": server_info.player_count,
-            "max_players": server_info.max_players,
-            "bot_count": server_info.bot_count,
-            "server_type": server_info.server_type,
-            "platform": server_info.platform,
-            "password_protected": server_info.password_protected,
-            "vac_enabled": server_info.vac_enabled,
-            "version": server_info.version,
-            "edf": server_info.edf,
-            "game_id": server_info.game_id,
-            "keywords": server_info.keywords,
+            "server_info": server_info,
             "player_list": players,
             "rules": rules
         }
